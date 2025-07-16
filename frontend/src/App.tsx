38ac181983dd8f87680dd9b377325338ad6f5e77
Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2025-07-16 15:44:10 +0700

    feat: update development linter and code style
    Signed-off-by: Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
-function App() {
-  return <h1>Hello World!</h1>
-}
+const App = () => (
+  <div className="flex h-full min-h-screen w-full items-center justify-center">
+    <h1 className="text-4xl font-semibold text-slate-800">Suitmedia!</h1>
+  </div>
+)
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,5 @@
-import { createRoot } from 'react-dom/client'
 import App from './App'
+import { createRoot } from 'react-dom/client'
+import '@/assets/styles.css'
 
 createRoot(document.getElementById('root')!).render(<App />)
